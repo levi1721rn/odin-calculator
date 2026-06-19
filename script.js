32f086c2930324411227159b411c23e3b1f9d175
nitheshraj <ipnitheshraj21@gmail.com>
2026-06-19 11:49:34 +0000

    added functions to store and display the numbers upon clicking.

--- script.js
+++ script.js
@@ -36,3 +36,65 @@ function operate(operator){
             return "";
     }
 }
+
+let display = document.querySelector('.panel');
+
+let one = document.querySelector('.one');
+one.addEventListener('click' , () => {
+    number1 = 1;
+    display.textContent = '1';
+})
+
+let two = document.querySelector('.two');
+two.addEventListener('click' , () => {
+    number1 = 2;
+    display.textContent = '2';
+})
+
+let three = document.querySelector('.three');
+three.addEventListener('click' , () => {
+    number1 = 3;
+    display.textContent = '3';
+})
+
+let four = document.querySelector('.four');
+four.addEventListener('click' , () => {
+    number1 = 4;
+    display.textContent = '4';
+})
+
+let five = document.querySelector('.five');
+five.addEventListener('click' , () => {
+    number1 = 5;
+    display.textContent = '5';
+})
+
+let six = document.querySelector('.six');
+six.addEventListener('click' , () => {
+    number1 = 6;
+    display.textContent = '6';
+})
+
+let seven = document.querySelector('.seven');
+seven.addEventListener('click' , () => {
+    number1 = 7;
+    display.textContent = '7';
+})
+
+let eight = document.querySelector('.eight');
+eight.addEventListener('click' , () => {
+    number1 = 8;
+    display.textContent = '8';
+})
+
+let nine = document.querySelector('.nine');
+nine.addEventListener('click' , () => {
+    number1 = 9;
+    display.textContent = '9';
+})
+
+let zero = document.querySelector('.zero');
+zero.addEventListener('click' , () => {
+    number1 = 0;
+    display.textContent = '0';
+})
